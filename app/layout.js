8cbe69402ac9f358da9f1f356ec2f5e98b8aac54
Michael Dowdle <michael.dowdle@hotmail.com>
2025-06-06 20:07:58 +0100

    Remove app folder to fix router conflict

--- app/layout.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css';
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="font-sans bg-gray-50 text-gray-900">{children}</body>
-    </html>
-  );
-}
